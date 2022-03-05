301fd3b74b3b7c273c132b6184021fbf7b3d0c72
Rey Vincent Canlas <94040143+vinxentience@users.noreply.github.com>
2022-03-05 17:47:44 +0800

    Add files via upload

---
+++ CCE105 - Data Structures and Algorithms/DS and Sorting/StackLinkedList.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Skye
+ */
+public class StackLinkedList {
+    
+}
+
+class StackAsLinkedList { 
+  
+    StackNode root; 
+  
+    static class StackNode { 
+        int data; 
+        StackNode next; 
+  
+        StackNode(int data) 
+        { 
+            this.data = data; 
+        } 
+    } 
+  
+    public boolean isEmpty() 
+    { 
+        if (root == null) { 
+            return true; 
+        } 
+        else
+            return false; 
+    } 
+  
+    public void push(int data) 
+    { 
+        StackNode newNode = new StackNode(data); 
+  
+        if (root == null) { 
+            root = newNode; 
+        } 
+        else { 
+            StackNode temp = root; 
+            root = newNode; 
+            newNode.next = temp; 
+        } 
+        System.out.println(data + " pushed to stack"); 
+    } 
+  
+    public int pop() 
+    { 
+        int popped = Integer.MIN_VALUE; 
+        if (root == null) { 
+            System.out.println("Stack is Empty"); 
+        } 
+        else { 
+            popped = root.data; 
+            root = root.next; 
+        } 
+        return popped; 
+    } 
+  
+    public int peek() 
+    { 
+        if (root == null) { 
+            System.out.println("Stack is empty"); 
+            return Integer.MIN_VALUE; 
+        } 
+        else { 
+            return root.data; 
+        } 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+  
+        StackAsLinkedList sll = new StackAsLinkedList(); 
+  
+        sll.push(10); 
+        sll.push(20); 
+        sll.push(30); 
+  
+        System.out.println(sll.pop() + " popped from stack"); 
+  
+        System.out.println("Top element is " + sll.peek()); 
+    } 
+} 
